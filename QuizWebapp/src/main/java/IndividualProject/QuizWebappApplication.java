339d3850bdac420705a9bd0354f32ddfae261311
AnteCF <ante.c.f.2002@gmail.com>
2022-09-13 15:27:03 +0200

    fixed some springboot stuff and added a yml file

---
+++ QuizWebapp/src/main/java/IndividualProject/QuizWebappApplication.java
@@ -0,0 +1,13 @@
+package IndividualProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizWebappApplication.class, args);
+	}
+
+}
---
+++ QuizWebapp/src/test/java/IndividualProject/QuizWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package IndividualProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
