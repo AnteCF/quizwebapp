3923cd20665b36f53935a4a7c56c9f34c1728522
AnteCF <ante.c.f.2002@gmail.com>
2022-09-16 13:44:41 +0200

    added some unit tests

--- QuizWebapp/src/test/java/IndividualProject/QuizWebappApplicationTests.java
+++ QuizWebapp/src/test/java/IndividualProject/QuizWebappApplicationTests.java
@@ -1,9 +1,7 @@
 package IndividualProject;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class QuizWebappApplicationTests {
 
 	@Test
